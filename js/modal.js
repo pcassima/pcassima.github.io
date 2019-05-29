6112a6819d94420009087f087084128f1cc2c0b3
Pieter-Jan Cassiman <pieterjan.cassiman@gmail.com>
2019-05-29 20:33:35 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,15 @@
 function close_modal(id){
+    // Find the modal with the given ID
     let image_modal = document.getElementById(id);
+    // Remove the open-modal class from the modal
     image_modal.classList.remove("open-modal");
-    console.log('closing modal');
 
 }
 
 function open_modal(id){
+    // Find the modal with the given ID
     let image_modal = document.getElementById(id);
+    // Add the open-modal class to the modal
     image_modal.classList.add("open-modal");
-    console.log('opening modal');
 
 }
